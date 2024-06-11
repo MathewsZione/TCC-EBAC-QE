3eeb22ea9a17e830ff698f7a4b865c8838c38b3e
Matheus Gonçalves Dos Santos Silva <matheusgoncalvesdossantossilva@gmail.com>
2024-06-11 00:06:04 -0300

    configurando projeto com dependencias e reporte

---
+++ UI/cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+    allure: {
+      outputDir: 'cypress/reports/allure-results',
+      disableWebdriverStepsReporting: false,
+      disableWebdriverScreenshotsReporting: false
+    },
+  },
+});
